e9d5ea16c1e99c894d594f08a9fae1891f3292e1
Jasper Koppen <jasperkoppen@gmail.com>
2018-10-10 12:05:26 +0200

    ping v0.1

--- lab6.py
+++ lab6.py
@@ -22,6 +22,42 @@ def random_position(n):
     return (x, y)
 
 
+def parse(peer, mcast, w, mcast_addr, sensor_pos, sensor_strength, sensor_decay,
+         grid_size, ping_period):
+    input = w.getline()
+    mcast.setblocking(0)
+    if (input == "ping"):
+        ping(mcast, peer, sensor_pos, sensor_decay, sensor_strength)
+
+    ready = select.select([mcast], [], [], 1)
+    if ready[0]:
+        data = mcast.recv(4096)
+        data = data.decode()
+        w.writeln(data)
+
+
+def get_distance(x, y):
+    print("nog niks ermee gedaan")
+
+# v-d^(x)
+def ping(mcast, peer, pos, decay, strength):
+    """
+    v = strength
+    x = decay
+    d = distance a tot b
+    """
+    # peer.sendto(bytes("Hello World", "utf-8"), ('127.0.0.1', 50000))
+    # peer.sendto(bytes("Hello World", "utf-8"), ('', 50000))
+    message = "Ping sent!!!!!\n"
+    message += "   From position: " + str(pos) + "\n"
+    message += "               x: " + str(pos[0]) + "\n"
+    message += "               y: " + str(pos[1]) + "\n"
+    message += "   Decay        : " + str(decay) + "\n"
+    message += "   Strength     : " + str(strength) + "\n"
+    peer.sendto(bytes(message, "utf-8"), (mcast_addr[0], 50000))
+
+
+
 def main(mcast_addr, sensor_pos, sensor_strength, sensor_decay,
          grid_size, ping_period):
     """
@@ -73,15 +109,10 @@ def main(mcast_addr, sensor_pos, sensor_strength, sensor_decay,
     Hier moeten wij gaan programmeren.
     """
     # # print(peer.sendto(bytes("Hello World", "utf-8"), ('127.0.0.1', 10000)))
-    peer.sendto(bytes("Hello World", "utf-8"), (mcast_addr[0], 50000))
-    while w.update():
-        mcast.setblocking(0)
 
-        ready = select.select([mcast], [], [], 1)
-        if ready[0]:
-            data = mcast.recv(4096)
-            data = data.decode()
-            w.writeln(data)
+    while w.update():
+        parse(peer, mcast, w, mcast_addr, sensor_pos, sensor_strength, sensor_decay,
+                 grid_size, ping_period)
 
         pass
 
@@ -99,7 +130,7 @@ if __name__ == '__main__':
     p.add_argument('--strength', help='sensor strength', default=50, type=int)
     p.add_argument('--decay', help='decay rate', default=1, type=int)
     p.add_argument('--period', help='period between autopings (0=off)',
-                   default=5, type=int)
+                   default=10, type=int)
     args = p.parse_args(sys.argv[1:])
     if args.pos:
         pos = tuple(int(n) for n in args.pos.split(',')[:2])
